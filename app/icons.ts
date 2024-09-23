d2aa752dcb8f39f6bb3ea4892ac9a920c5b159d3
_Nik0__ <maciek.f137@gmail.com>
2024-09-24 00:32:41 +0200

    Another test lol

---
+++ app/icons.ts
@@ -0,0 +1,21 @@
+// icons.js
+import { 
+    AcademicCapIcon,
+    CodeBracketIcon,
+    CodeBracketSquareIcon,
+    DivideIcon,
+    DocumentCurrencyBangladeshiIcon,
+    FolderArrowDownIcon,
+    PlusIcon,
+    ServerIcon,
+    // ... import all other icons you need
+  } from '@heroicons/react/24/solid';
+  
+  export const icons = {
+    server: ServerIcon,
+    install: FolderArrowDownIcon,
+    plus: PlusIcon,
+    plug: CodeBracketSquareIcon,
+    // ... map all other icons
+  };
+  
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.nikostuff.com',
+        port: '',
+        pathname: '/v1/img/uploads/container_ec46113a1b8b38bd89c0729ef099582b/**',
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
