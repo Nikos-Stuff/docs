ba9162b55e50f8ca35fbad62313060dd90eb704d
_Nik0__ <maciek.f137@gmail.com>
2024-12-03 01:31:29 +0100

    🔌 Fully fix bugs

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,5 +1,4 @@
 import { source } from '@/lib/source';
-import { createMetadata } from '@/lib/metadata';
 import {
   DocsPage,
   DocsBody,
@@ -9,23 +8,24 @@ import {
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 
-
-export default async function Page({
-  params,
-}: {
-  params: { slug?: string[] };
+export default async function Page(props: {
+  params: Promise<{ slug?: string[] }>;
 }) {
-  const params_w = await params
-  const page = source.getPage(params_w.slug);
+  const params = await props.params;
+  const page = source.getPage(params.slug);
   if (!page) notFound();
 
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} tableOfContent={{
-      style: 'clerk',
-      single: false,
-    }} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      tableOfContent={{
+        style: 'clerk',
+        single: false,
+      }}
+      full={page.data.full}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
@@ -39,13 +39,15 @@ export async function generateStaticParams() {
   return source.generateParams();
 }
 
-export async function generateMetadata({ params }: { params: { slug?: string[] } }) {
-  const params_w = await params
-  const page = source.getPage(params_w.slug);
+export async function generateMetadata(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
   if (!page) notFound();
- 
-  return createMetadata({
+
+  return {
     title: page.data.title,
     description: page.data.description,
-  });
+  };
 }
