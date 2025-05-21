5be0466725bf8caea0a3655ebed653ec806de6b7
_Nik0__ <maciek.f137@gmail.com>
2025-05-21 19:23:25 +0200

    🥚 Egg

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const config = {
   reactStrictMode: true,
   output: 'export',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  //trailingSlash: true,
+  trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   //skipTrailingSlashRedirect: true,
 };
