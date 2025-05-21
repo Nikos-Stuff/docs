99ac251bbc395885cf0e19df3f784941f7b97d3f
_Nik0__ <maciek.f137@gmail.com>
2025-05-21 19:27:53 +0200

    🥚🥚 So many eggs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const config = {
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  //skipTrailingSlashRedirect: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(config);
