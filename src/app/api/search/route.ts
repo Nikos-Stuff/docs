0ba47e44e305393b7b2cd848f8fbb81d35e56cb8
_Nik0__ <maciek.f137@gmail.com>
2025-05-21 07:55:40 +0200

    🦴 Yesn't

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,33 +1,7 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
-import { generateOGImage } from 'fumadocs-ui/og';
-import { notFound } from 'next/navigation';
 
+// it should be cached forever
 export const revalidate = false;
 
-// Prepare helper functions
-const { GET: staticSearchGET } = createFromSource(source);
-
-export async function GET(
-  req: Request,
-  { params }: { params: { slug: string[] } },
-) {
-  const { slug } = params;
-
-  const isImageRequest = slug[slug.length - 1] === 'image.png';
-
-  if (isImageRequest) {
-    const pageSlug = slug.slice(0, -1);
-    const page = source.getPage(pageSlug);
-    if (!page) return notFound();
-
-    return generateOGImage({
-      title: page.data.title,
-      description: page.data.description,
-      site: "Niko's Stuff - Docs",
-    });
-  }
-
-  // Fallback to static search handler
-  return staticSearchGET(req);
-}
+export const { staticGET: GET } = createFromSource(source);
