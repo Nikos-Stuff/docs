760890bf1f7224f66a1e9c4aa24923b451061aae
_Nik0__ <maciek.f137@gmail.com>
2025-05-21 18:42:50 +0200

    🔧 skipTrailingSlashRedirect

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,9 @@ const config = {
   reactStrictMode: true,
   output: 'export',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
+  //trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  //skipTrailingSlashRedirect: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(config);
